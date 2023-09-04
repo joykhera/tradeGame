cec0171307838fbcfa2ee6a2ac029d50a03a68d3
joykhera <joykhera@gmail.com>
2023-09-04 12:09:56 -0400

    requests not working

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var ctx = document.getElementById('chart').getContext('2d');
 ctx.canvas.width = 1000;
 ctx.canvas.height = 250;
 
-var barData = getRandomData(initialDateStr, barCount);
+var barData = await getData(initialDateStr, barCount);
 console.log(barData)
 function lineData() { return barData.map(d => { return { x: d.x, y: d.c } }) };
 
@@ -19,39 +19,37 @@ var chart = new Chart(ctx, {
     }
 });
 
-var getRandomInt = function (max) {
-    return Math.floor(Math.random() * Math.floor(max));
-};
+async function getData() {
+    const ticker = 'AAPL'
+    const interval = '1m'
+    const start = '2023-08-11'
+    const end = '2023-08-12'
 
-function randomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
+    const response = await fetch('/api/getData', {
+    // const response = await fetch('http://127.0.0.1:5000/getData', {
+        method: 'POST',
+        mode: 'no-cors',
+        // headers: {
+        //     'Accept': 'application/json',
+        //     'Content-Type': 'application/json'
+        // },
+        // body: JSON.stringify({ ticker, start, end, interval })
+        body: JSON.stringify({ ticker: ticker, start: start, end: end, interval: interval })
+    });
 
-function randomBar(date, lastClose) {
-    var open = +randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
-    var close = +randomNumber(open * 0.95, open * 1.05).toFixed(2);
-    var high = +randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
-    var low = +randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
-    return {
-        x: date.valueOf(),
-        o: open,
-        h: high,
-        l: low,
-        c: close
-    };
-
-}
-
-function getRandomData(dateStr, count) {
-    var date = luxon.DateTime.fromRFC2822(dateStr);
-    var data = [randomBar(date, 30)];
-    while (data.length < count) {
-        date = date.plus({ days: 1 });
-        if (date.weekday <= 5) {
-            data.push(randomBar(date, data[data.length - 1].c));
-        }
+    const tickerData = await response.json();
+    // console.log('content', data)
+    const chartData = []
+    for (const [date, data] of Object.entries(tickerData)) {
+        chartData.push({
+            x: date,
+            o: data['open'],
+            h: data['high'],
+            l: data['low'],
+            c: data['close']
+        })
     }
-    return data;
+    return chartData;
 }
 
 var update = function () {
--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 import yfinance as yf
-from flask import Flask, render_template, request
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import json
+
 app = Flask(__name__)
-
-
-# @app.route('/')
-# def home():
-#    return render_template('index.html')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/getData', methods=['POST'])
+@cross_origin()
 def get_ticker_data():
     print(request.json)
     ticker = request.json['ticker']
     start = request.json['start']
     end = request.json['end']
     interval = request.json['interval']
+    
     data = yf.download(ticker, start=start, end=end, interval=interval)
     data_dict = data.to_dict(orient='index')
     formatted_data = {}
+    
     for key, value in data_dict.items():
-        # print(key, type(key), key.timestamp())
-        # print(key, int(round(key * 1000)))
         formatted_data[str(round(key.timestamp() * 1000))] = value
-    # print(data_dict)
-    # return json.dumps(data.to_dict(orient='index'))
-    # return data.to_json(orient='columns')
-    return formatted_data
-
-# data = yf.download("SPY", start="2023-08-11", end="2023-08-12", interval="1m")
-
-# print(data.head())
-
+    return json.dumps(formatted_data)
 
 if __name__ == '__main__':
    app.run()
