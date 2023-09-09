6815cfcc5d8f4e1c20808adb2ab85d87d7728217
joykhera <joykhera@gmail.com>
2023-09-09 10:35:34 -0400

    randomize ticker and date

--- static/index.js
+++ static/index.js
@@ -1,20 +1,41 @@
 const ctx = document.getElementById('chart').getContext('2d');
 ctx.canvas.width = 1000;
 ctx.canvas.height = 250;
+
 const curDataLen = 50;
+const tickerOptions = ['TSLA', 'AAPL', 'F', 'NVDA', 'AMD']
+const today = new Date()
+const latestDate = new Date()
+latestDate.setDate(today.getDate() - 1)
+const earliestDate = new Date()
+earliestDate.setDate(today.getDate() - 30)
+const timeDiff = latestDate.getTime() - earliestDate.getTime();
 
 class StockChart {
-    constructor() {
+    constructor(){
+        this.ticker = tickerOptions[Math.floor(Math.random() * tickerOptions.length)]
+        let day = 0;
+
+        while (day < 1 || day > 5) {
+            const randomTime = Math.random() * timeDiff;
+            const randomDate = new Date(earliestDate.getTime() + randomTime)
+            this.startDate = randomDate.toISOString().split('T')[0]
+            this.endDate = new Date(randomDate.getTime() + 86400000).toISOString().split('T')[0]
+            day = randomDate.getDay();
+        }
+
         this.chart = new Chart(ctx, {
             type: 'candlestick',
             data: {
                 datasets: [{
-                    label: 'CHRT - Chart.js Corporation',
+                    label: this.ticker + ' ' + this.startDate,
                     data: this.barData
                 }]
             },
         })
+
         this.getData().then(data => this.barData = data).then(() => this.update())
+        console.log(this.ticker, this.startDate, this.endDate)
     }
 
     lineData = () => { return this.barData.map(d => { return { x: d.x, y: d.c } }) }
@@ -31,7 +52,8 @@ class StockChart {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ ticker: ticker, start: start, end: end, interval: interval })
+            // body: JSON.stringify({ ticker: ticker, start: start, end: end, interval: interval })
+            body: JSON.stringify({ ticker: this.ticker, start: this.startDate, end: this.endDate, interval: interval })
         });
 
         const tickerData = await response.json();
@@ -68,16 +90,18 @@ class StockChart {
         // mixed charts
         this.chart.config.data.datasets = [
             {
-                label: 'CHRT - Chart.js Corporation',
+                label: this.ticker + ' ' + this.startDate,
                 data: data
             },
         ]
         // console.log(this.chart.config.data.datasets)
- 
+
         this.chart.update();
-        // window.requestAnimationFrame(() => this.update(index + 1));
-        console.log(index)
-        setTimeout(() => this.update(index + 1), 500);
+
+        if (index <= this.barData.length) {
+            console.log(index)
+            setTimeout(() => this.update(index + 1), 500);
+        }
     }
 };
 
