db3f9057c853c06c367ad2b14594482b2cadf2c8
joykhera <joykhera@gmail.com>
2023-09-09 09:31:46 -0400

    data now flows on chart

--- static/index.js
+++ static/index.js
@@ -1,10 +1,10 @@
 const ctx = document.getElementById('chart').getContext('2d');
 ctx.canvas.width = 1000;
 ctx.canvas.height = 250;
+const curDataLen = 50;
 
 class StockChart {
     constructor() {
-        // this.barData = await this.getData(initialDateStr, barCount)
         this.chart = new Chart(ctx, {
             type: 'candlestick',
             data: {
@@ -15,8 +15,6 @@ class StockChart {
             },
         })
         this.getData().then(data => this.barData = data).then(() => this.update())
-        // this.getData(initialDateStr, barCount).then(data => console.log(data))
-
     }
 
     lineData = () => { return this.barData.map(d => { return { x: d.x, y: d.c } }) }
@@ -28,20 +26,16 @@ class StockChart {
         const end = '2023-08-12'
 
         const response = await fetch('/getData', {
-            // const response = await fetch('http://127.0.0.1:5000/getData', {
             method: 'POST',
-            // mode: 'no-cors',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            // body: JSON.stringify({ ticker, start, end, interval })
             body: JSON.stringify({ ticker: ticker, start: start, end: end, interval: interval })
         });
 
         const tickerData = await response.json();
-        // console.log('tickerData', tickerData)
-        // console.log('content', data)
+
         const chartData = []
         for (const [date, data] of Object.entries(tickerData)) {
             chartData.push({
@@ -55,8 +49,8 @@ class StockChart {
         return chartData;
     }
 
-    update = () => {
-        var dataset = this.chart.config.data.datasets[0];
+    update = (index = 0) => {
+        const dataset = this.chart.config.data.datasets[0];
         this.chart.config.options.scales.y.type = 'linear';
 
         dataset.color = {
@@ -66,56 +60,24 @@ class StockChart {
         };
 
         // border
-        var defaultOpts = Chart.defaults.elements['candlestick'];
+        const defaultOpts = Chart.defaults.elements['candlestick'];
         dataset.borderColor = defaultOpts.borderColor;
+        const data = this.barData.filter((d, i) => i >= index - curDataLen && i < index);
+        // console.log(index, this.barData, data)
 
         // mixed charts
         this.chart.config.data.datasets = [
             {
                 label: 'CHRT - Chart.js Corporation',
-                data: this.barData
+                data: data
             },
-            // {
-            //     label: 'Close price',
-            //     type: 'line',
-            //     data: this.lineData()
-            // }
         ]
-        console.log(this.chart.config.data.datasets)
+        // console.log(this.chart.config.data.datasets)
  
-        const delayBetweenPoints = 10000 / this.barData.length;
-        const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
-        const animationOptions = {
-            x: {
-                type: 'number',
-                easing: 'linear',
-                duration: delayBetweenPoints,
-                from: NaN,
-                delay(ctx) {
-                    if (ctx.type !== 'data' || ctx.xStarted) {
-                        return 0;
-                    }
-                    ctx.xStarted = true;
-                    console.log(ctx.index, ctx.index * delayBetweenPoints)
-                    return ctx.index * delayBetweenPoints;
-                }
-            },
-            y: {
-                type: 'number',
-                easing: 'linear',
-                duration: delayBetweenPoints,
-                from: previousY,
-                delay(ctx) {
-                    if (ctx.type !== 'data' || ctx.yStarted) {
-                        return 0;
-                    }
-                    ctx.yStarted = true;
-                    return ctx.index * delayBetweenPoints;
-                }
-            }
-        }
-        this.chart.config.options.animation = animationOptions
         this.chart.update();
+        // window.requestAnimationFrame(() => this.update(index + 1));
+        console.log(index)
+        setTimeout(() => this.update(index + 1), 500);
     }
 };
 
